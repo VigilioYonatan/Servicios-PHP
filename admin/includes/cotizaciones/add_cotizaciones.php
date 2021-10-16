82dedeac5a2d85c97b4d488c425b4b950f8556b3
Vigilio <yonatanvigiliolavado09@gmail.com>
2021-10-15 21:34:18 -0500

    cambios

--- admin/includes/cotizaciones/add_cotizaciones.php
+++ admin/includes/cotizaciones/add_cotizaciones.php
@@ -10,34 +10,72 @@ echo "<script>window.open('index.php?logged_in=Logueaste%20correctamente!','_sel
     <input type="text">
     <label>Estado:</label>
     <select name="estado" id="">
-        <option >Selecciona uno</option>
-        <option value="abierto">Abierto</option>
-        <option value="pendiente">Pendiente</option>
-        <option value="proceso">Proceso</option>
-        <option value="cerrado">Cerrado</option>
+        <option >Selecciona un estado</option>
+    <?php
+        $get_cats = "select * from venta_estado";
+
+        $run_cats = mysqli_query($conexion, $get_cats);
+
+        while ($row_cats = mysqli_fetch_array($run_cats)) {
+            $estado_id = $row_cats['est_id'];
+            $estado_nombre = $row_cats['est_nombre'];
+                    
+            echo "<option value='$estado_id'>$estado_nombre</option>";
+        }
+    ?>
     </select><br>
     <label>Asignado:</label>
     <input type="text" name="asigando">
     <label>Forma de pago</label>
     <select name="pago">
-        <option >Selecciona un pago</option>
-        <option value="efectivo">Efectivo</option>
-        <option value="10">10 dias</option>
-        <option value="15">15 dias</option>
-        <option value="20">20 dias</option>
+        <option >Selecciona un tipo pago</option>
+    <?php
+        $get_cats = "select * from venta_pago";
+
+        $run_cats = mysqli_query($conexion, $get_cats);
+
+        while ($row_cats = mysqli_fetch_array($run_cats)) {
+            $pago_id = $row_cats['pago_id'];
+            $pago_nombre = $row_cats['pago_nombre'];
+                    
+            echo "<option value='$pago_id'>$pago_nombre</option>";
+        }
+    ?>
     </select><br>
     <label>Moneda:</label>
     <select name="moneda">
-        <option >Selecciona un pago</option>
-        <option>Efectivo</option>
-        <option value="soles">Soles</option>
-        <option value="dolares">Dolares</option>
-        <option value="euros">Euros</option>
+        <option >Selecciona un tipo de moneda</option>
+        <?php
+        $get_cats = "select * from venta_moneda";
+
+        $run_cats = mysqli_query($conexion, $get_cats);
+
+        while ($row_cats = mysqli_fetch_array($run_cats)) {
+            $moneda_id = $row_cats['moneda_id'];
+            $moneda_nombre = $row_cats['moneda_nombre'];
+                    
+            echo "<option value='$moneda_id'>$moneda_nombre</option>";
+        }
+    ?>
     </select>
     <label>Tiempo de entrega: </label>
     <input type="number" name="tiempo"><br>
     <label>Expira: </label>
-    <input type="number" name="expira">
+    <select name="expira" id="">
+        <option >Selecciona el tiempo de expiracion</option>
+    <?php
+        $get_cats = "select * from venta_expira";
+
+        $run_cats = mysqli_query($conexion, $get_cats);
+
+        while ($row_cats = mysqli_fetch_array($run_cats)) {
+            $expira_id = $row_cats['expira_id'];
+            $expira_nombre = $row_cats['expira_nombre'];
+                    
+            echo "<option value='$expira_id'>$expira_nombre</option>";
+        }
+    ?>
+    </select>
     <label>Dirección: </label>
     <input type="text" name="direccion"><br>
     <label>Condiciones generales: </label>
